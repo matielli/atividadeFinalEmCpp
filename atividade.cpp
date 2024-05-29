6dca26799b1619bb46f6d9af601b0c8b0115016d
matielli <162385460+matielli@users.noreply.github.com>
2024-05-29 00:05:35 +0000

    atualizado

--- atividade.cpp
+++ atividade.cpp
@@ -34,9 +34,9 @@ if (menu == 1){
     cin >> Titulo[controle];
     cout << "Descrição:\n ";
     cin >> Descricao[controle];
-    cout << "Data De Vencimento:\n ";
+    cout << "Data De Vencimento (DD/MM/AAAA):\n ";
     cin >> DataDeVencimento[controle];
-    cout << "Status:\n ";
+    cout << "Status (1 - Pendente, 2 - Em Progresso, 3 - Concluída):\n ";
     cin >> Status[controle];
      controle ++;
 }
@@ -45,28 +45,52 @@ if (menu == 1){
 if (menu == 2){
 cout <<"Lista De Tarefas" << endl;
     for (int i = 0; i < controle; i++){
-        cout << i;
-        cout << "ID: " << ID[i]  << endl;
+        cout << "ID:" << ID[i]  << endl;
         cout << "Titulo: " << Titulo[i] << endl;
         cout << "Descricao: " << Descricao[i] << endl;
-        cout << "DataDeVencimento: " << DataDeVencimento[i] << endl;
-        cout << "Status: " << Status[i] << endl;
+        cout << "DataDeVencimento (DD/MM/AAAA): " << DataDeVencimento[i] << endl;
+        cout << "Status (1 - Pendente, 2 - Em Progresso, 3 - Concluída): " << Status[i] << endl;
     }
 
     
 }
 if (menu == 3){
-    
+
+    int editar;
+    cout << "Digite o ID da tarefa para ser editada.\n";
+    cin >> editar;
+            cout << "Novo Título: ";
+            cin >> Titulo[editar];
+            cout << "Nova Descrição: ";
+            cin >> Descricao[editar];
+            cout << "Nova Data de Vencimento (DD/MM/AAAA): ";
+            cin >> DataDeVencimento[editar];
+            cout << "Novo Status (1 - Pendente, 2 - Em Progresso, 3 - Concluída): ";
+            cin >> Status[editar];
+        
+            cout << "Tarefa editada com sucesso!" << endl;
+        
 
     }
 
-    }while (menu !=0);
+    if (menu == 4){
+        int apagar;
+        cout << "Digite o ID que voce quer apagar.\n";
+        cin >> apagar;
+
+        cout << "Tarefa apagada com sucesso!" << endl;
+    }
+
+    if (menu == 5){
+        int buscar;
+        
     
+    
+    
+    }
 
-   return 0;
-
+}while (menu !=0);
+    
+    return 0;
+   
 }
-
-
-
-
